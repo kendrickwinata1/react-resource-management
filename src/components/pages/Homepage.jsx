565b4ee4249cbe71adff1ffe56094ab51d4a40c9
Kendrick Winata <kendrickwinata@outlook.com>
2022-12-08 21:54:31 +0800

    feat: add in fullcalendar

--- src/components/pages/Homepage.jsx
+++ src/components/pages/Homepage.jsx
@@ -1,126 +1,22 @@
 import React from "react";
 import FullCalendar, { formatDate } from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
-import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
+import timeGridPlugin from "@fullcalendar/timegrid";
 import { INITIAL_EVENTS, createEventId } from "../../event-utils";
 import { Form, Button, Row, Col, Table } from "react-bootstrap";
 
-export default class App extends React.Component {
-	state = {
-		weekendsVisible: true,
-		currentEvents: [],
-	};
+import { useState } from "react";
+import { v4 as uuid } from "uuid";
 
-	handleWeekendsToggle = () => {
-		this.setState({
-			weekendsVisible: !this.state.weekendsVisible,
-		});
-	};
-
-	handleDateSelect = (selectInfo) => {
-		let title = prompt("Please enter a new title for your event");
-		let calendarApi = selectInfo.view.calendar;
-
-		calendarApi.unselect(); // clear date selection
-
-		if (title) {
-			calendarApi.addEvent({
-				id: createEventId(),
-				title,
-				start: selectInfo.startStr,
-				end: selectInfo.endStr,
-				allDay: selectInfo.allDay,
-				color: "",
-			});
-		}
-	};
-
-	handleEventClick = (clickInfo) => {
-		if (
-			window.confirm(
-				`Are you sure you want to delete the event '${clickInfo.event.title}'`
-			)
-		) {
-			clickInfo.event.remove();
-		}
-	};
-
-	handleEvents = (events) => {
-		this.setState({
-			currentEvents: events,
-		});
-	};
-
-	render() {
-		return (
-			<div>
-				<div className="app">
-					<div className="sidebar">
-						<div className="sidebar-section">
-							<h3>Instructions</h3>
-							<ul>
-								<li>
-									Select dates and you will be prompted to create a new event
-								</li>
-								<li>Drag, drop, and resize events</li>
-								<li>Click an event to delete it</li>
-							</ul>
-							<Form>
-								<Form.Group controlId="name">
-									<Form.Label>Name</Form.Label>
-									<Form.Control
-										type="text"
-										placeholder="Enter Name"
-										className="form-input"
-									/>
-								</Form.Group>
-
-								<Button type="submit" className="button my-3">
-									Update
-								</Button>
-							</Form>
-						</div>
-						<div className="sidebar-section">
-							<label>
-								<input
-									type="checkbox"
-									checked={this.state.weekendsVisible}
-									onChange={this.handleWeekendsToggle}></input>
-								toggle weekends
-							</label>
-						</div>
-						<div className="sidebar-section">
-							<h3>All Events ({this.state.currentEvents.length})</h3>
-							<ul>{this.state.currentEvents.map(renderSidebarEvent)}</ul>
-						</div>
-					</div>
-					<div className="main">
-						<FullCalendar
-							plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-							headerToolbar={{
-								left: "prev,next today",
-								center: "title",
-								right: "dayGridMonth,timeGridWeek,timeGridDay",
-							}}
-							initialView="dayGridMonth"
-							editable={true}
-							selectable={true}
-							selectMirror={true}
-							dayMaxEvents={true}
-							weekends={this.state.weekendsVisible}
-							initialEvents={INITIAL_EVENTS}
-							select={this.handleDateSelect}
-							eventContent={renderEventContent}
-							eventClick={this.handleEventClick}
-							eventsSet={this.handleEvents}
-						/>
-					</div>
-				</div>
-			</div>
-		);
-	}
-}
+const EventItem = ({ info }) => {
+	const { event } = info;
+	return (
+		<div>
+			<p>{event.title}</p>
+		</div>
+	);
+};
 
 function renderEventContent(eventInfo) {
 	return (
@@ -145,3 +41,118 @@ function renderSidebarEvent(event) {
 		</li>
 	);
 }
+
+function Homepage() {
+	const currentState = {
+		weekendsVisible: true,
+		currentEvents: [],
+	};
+	const [events, setEvents] = useState([]);
+	const [state, setState] = useState(currentState);
+
+	const handleWeekendsToggle = () => {
+		this.setState({
+			weekendsVisible: !state.weekendsVisible,
+		});
+	};
+
+	const handleDateSelect = (selectInfo) => {
+		let title = prompt("Please enter a new title for your event");
+		let calendarApi = selectInfo.view.calendar;
+
+		calendarApi.unselect(); // clear date selection
+
+		if (title) {
+			calendarApi.addEvent({
+				id: createEventId(),
+				title,
+				start: selectInfo.startStr,
+				end: selectInfo.endStr,
+				allDay: selectInfo.allDay,
+				category: "",
+			});
+		}
+	};
+
+	const handleEventClick = (clickInfo) => {
+		if (
+			window.confirm(
+				`Are you sure you want to delete the event '${clickInfo.event.title}'`
+			)
+		) {
+			clickInfo.event.remove();
+		}
+	};
+
+	const handleEvents = (events) => {
+		setState({
+			currentEvents: events,
+		});
+	};
+
+	return (
+		<div className="app">
+			<div className="sidebar">
+				<div className="sidebar-section">
+					<h3>Instructions</h3>
+					<ul>
+						<li>Select dates and you will be prompted to create a new event</li>
+						<li>Drag, drop, and resize events</li>
+						<li>Click an event to delete it</li>
+					</ul>
+					<Form>
+						<Form.Group controlId="name">
+							<Form.Label>Name</Form.Label>
+							<Form.Control
+								type="text"
+								placeholder="Enter Name"
+								className="form-input"
+							/>
+						</Form.Group>
+
+						<Button type="submit" className="button my-3">
+							Update
+						</Button>
+					</Form>
+				</div>
+				<div className="sidebar-section">
+					<label>
+						<input
+							type="checkbox"
+							checked={state.weekendsVisible}
+							onChange={handleWeekendsToggle}></input>
+						toggle weekends
+					</label>
+				</div>
+				<div className="sidebar-section">
+					<h3>All Events ({state.currentEvents.length})</h3>
+					<ul>{state.currentEvents.map(renderSidebarEvent)}</ul>
+				</div>
+			</div>
+			<div>
+				<FullCalendar
+					plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+					headerToolbar={{
+						left: "prev,next today",
+						center: "title",
+						right: "dayGridMonth,timeGridWeek,timeGridDay",
+					}}
+					initialView="dayGridMonth"
+					editable={true}
+					selectable={true}
+					events={events}
+					select={handleDateSelect}
+					eventContent={renderEventContent}
+					eventClick={handleEventClick}
+					eventsSet={handleEvents}
+					weekends={state.weekendsVisible}
+					initialEvents={INITIAL_EVENTS}
+					eventContent={(info) => <EventItem info={info} />}
+					views={["dayGridMonth", "dayGridWeek", "dayGridDay"]}
+				/>
+			</div>
+		</div>
+	);
+}
+
+export default Homepage;
